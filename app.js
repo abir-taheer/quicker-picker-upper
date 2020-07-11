c5750faf617f6843fb72ba95b0420f1248bfb47e
Abir Taheer <abir@taheer.me>
2020-07-11 04:15:49 -0400

    exported middleware code out of app.js

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
-const database = require('./database');
-const opengraph = require('./opengraph');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
-const expressSession = require('express-session');
-const SequelizeConnectSession = require('connect-session-sequelize')(
-	expressSession.Store
-);
-const sequelizeStore = new SequelizeConnectSession({ db: database.sequelize });
 
-const sessionSecret =
-	process.env.SESSION_SECRET || 'some_semi_permanent_secret';
+const logger = require('./middleware/logger');
+app.use(logger);
 
-const session = expressSession({
-	secret: sessionSecret,
-	name: 'session',
-	resave: true,
-	saveUninitialized: false,
-	store: sequelizeStore,
-	cookie: {
-		path: '/',
-		httpOnly: true,
-		secure: false,
-		maxAge: 7 * 86400 * 1000
-	},
-	rolling: true
-});
-
-sequelizeStore.sync();
-
+const session = require('./middleware/session');
 app.use(session);
 
-app.use(cookieParser(sessionSecret));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const parsers = require('./middleware/parsers');
+app.use(parsers);
 
-app.use(
-	morgan(process.env.MORGAN_FORMAT || 'dev', {
-		skip: (req, res) => res.statusCode < 400
-	})
-);
-
+const opengraph = require('./opengraph');
 app.use(opengraph);
 
 // ROUTES
