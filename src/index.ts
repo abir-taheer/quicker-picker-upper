da3ac480b9b7eed248fa010bb4745f1cdd3dc348
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2022-02-18 16:46:16 -0500

    fixed typo of port keyword

--- src/index.ts
+++ src/index.ts
@@ -28,13 +28,13 @@ export async function run() {
       });
     } else {
       // Code to run inside of a worker
-      app.listen(port, () =>
-        console.log(`Worker ${cluster.worker.id} listening on port ${port}`)
+      app.listen(PORT, () =>
+        console.log(`Worker ${cluster.worker.id} listening on port ${PORT}`)
       );
     }
   } else {
     // Code to run in a development env
-    app.listen(PORT, () => console.log(`Listening on port ${port}`));
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
   }
 }
 
