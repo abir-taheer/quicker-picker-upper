b533336ae04d0156220d7030aa24b763275da343
Abir Taheer <40152590+abir-taheer@users.noreply.github.com>
2022-02-18 16:44:20 -0500

    import port from constants instead of redefining it

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 require('dotenv').config();
 import cluster, { Worker } from 'cluster';
 import setupApp from './app';
-
-const port: number = Number(process.env.PORT) || 3001;
+import { PORT } from "./constants";
 
 export async function run() {
   const app = await setupApp();
@@ -35,7 +34,7 @@ export async function run() {
     }
   } else {
     // Code to run in a development env
-    app.listen(port, () => console.log(`Listening on port ${port}`));
+    app.listen(PORT, () => console.log(`Listening on port ${port}`));
   }
 }
 
